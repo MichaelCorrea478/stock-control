0f5f99d9e110d0cfaf02bc34c4be5c5a85f0fbf3
MichaelCorrea478 <michaelcorrea.eb@gmail.com>
2022-12-27 01:12:58 -0300

    Inicio do projeto de carteira de ações

---
+++ database/migrations/2022_12_27_040632_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('transaction_type_id')->constrained('transactions');
+            $table->string('stock_symbol')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->decimal('value', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <h1 class="text-black-50">Você está logado!</h1>
+    </div>
+@endsection
